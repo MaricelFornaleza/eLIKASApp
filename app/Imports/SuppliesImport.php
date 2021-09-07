0863bc20a7731d8b4261bf7fb6881741c2e2636b
Sevsssssss <sevsarate28@gmail.com>
2021-09-07 12:46:28 +0800

    Inventory Supply Import

--- app/Imports/SuppliesImport.php
+++ app/Imports/SuppliesImport.php
@@ -31,6 +31,32 @@ class SuppliesImport implements ToCollection, WithHeadingRow
 
         foreach ($collection as $row) {
             if ($row->filter()->isNotEmpty()) {
+                $user_inventory_prev_stock = $user->user_inventory()->first();
+                if($row['supply_type'] == 'Food Packs'){
+                    $user->user_inventory()->update([
+                        'total_no_of_food_packs'                    => ($user_inventory_prev_stock->total_no_of_food_packs + $row['quantity']),
+                    ]);
+                }else if($row['supply_type'] == 'Water'){
+                    $user->user_inventory()->update([
+                        'total_no_of_water'                         => ($user_inventory_prev_stock->total_no_of_water + $row['quantity']),
+                    ]);
+                }else if($row['supply_type'] == 'Hygiene Kit'){
+                    $user->user_inventory()->update([
+                        'total_no_of_hygiene_kit'                   => ($user_inventory_prev_stock->total_no_of_hygiene_kit + $row['quantity']),
+                    ]);
+                }else if($row['supply_type'] == 'Medicine'){
+                    $user->user_inventory()->update([
+                        'total_no_of_medicine'                      => ($user_inventory_prev_stock->total_no_of_medicine + $row['quantity']),
+                    ]);
+                }else if($row['supply_type'] == 'Clothes'){
+                    $user->user_inventory()->update([
+                        'total_no_of_clothes'                       => ($user_inventory_prev_stock->total_no_of_clothes + $row['quantity']),
+                    ]);
+                }else if($row['supply_type'] == 'ESA'){
+                    $user->user_inventory()->update([
+                        'total_no_of_emergency_shelter_assistance'  => ($user_inventory_prev_stock->total_no_of_emergency_shelter_assistance + $row['quantity']),
+                    ]);
+                } 
                 $supply = new Supply();
                 $supply->inventory_id     = $user_inventory_id;
                 $supply->supply_type   = $row['supply_type'];
