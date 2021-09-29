25ae55cf5c1b546a23f35f226969c44a3cfaf38c
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-09-29 12:24:19 +0800

    SMS receive and reply

--- routes/web.php
+++ routes/web.php
@@ -221,11 +221,12 @@ Route::get('send', function () {
     $user = User::where('contact_no', '9772779609')->first();
     $access_token = $user->globe_labs_access_token;
     $response = $http->post("https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/0098/requests?access_token=" . $access_token, [
-        "senderAddress" => env('SHORT_CODE_SUFFIX'),
-        "clientCorrelator" => env('SHORT_CODE'),
-        "message" => "Text received",
-        "address" => "9772779609"
-
+        "outboundSMSMessageRequest" => [
+            "senderAddress" => env('SHORT_CODE_SUFFIX'),
+            "clientCorrelator" => env('SHORT_CODE'),
+            "outboundSMSTextMessage" => ["message" => "Text received"],
+            "address" => "9772779609"
+        ]
     ]);
     Log::info($response->getBody());
 });
