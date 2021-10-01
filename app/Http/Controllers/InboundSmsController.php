076db381b0647f9bae918f4a6db8bb647af3ecdb
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-10-01 15:46:40 +0800

    SMS - Cancel Request

--- app/Http/Controllers/InboundSmsController.php
+++ app/Http/Controllers/InboundSmsController.php
@@ -200,7 +200,7 @@ class InboundSmsController extends Controller
     public function cancelRequest($sender, $message)
     {
         $user = User::where('contact_no', $sender)->first();
-        $delivery_request = DeliveryRequest::where('camp_manager_id', '=', $user->id)->orderBy('id', 'desc')->first();
+        $delivery_request = DeliveryRequest::all();
         // $delivery_request->status = 'cancelled';
         // $delivery_request->save();
 
