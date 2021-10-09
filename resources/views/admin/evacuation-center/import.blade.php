f9f01849a05d5bccfc35636998b1314c69540143
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-10-09 10:44:03 +0800

    Import sample

--- resources/views/admin/evacuation-center/import.blade.php
+++ resources/views/admin/evacuation-center/import.blade.php
@@ -10,53 +10,85 @@
     <div class="fade-in">
 
         <div class="row center">
-            <div class="col-lg-8 ">
+            <div class="col-lg-10 ">
                 <div class="card">
                     <div class="card-header">
                         <h5>Import Excel to add Evacuation Centers</h5>
 
                     </div>
                     <div class="card-body ">
-                        <h6>To add more than one evacuation center, you can upload an excel file.</h6>
                         <div class="row">
                             @if(count($errors) > 0)
                             <div class="alert alert-danger col-12">
                                 <h6>
                                     Upload error
                                 </h6>
-
                             </div>
-
                             @endif
 
                         </div>
-                        <code>Important note: The file must contain the following column name.</code>
-                        <ul>
-
-                            <li><strong>evacuation_center_name</strong><code>*</code></li>
-                            <li><strong>address</strong><code>*</code></li>
-                            <li><strong>latitude</strong><code>*</code></li>
-                            <li><strong>longitude</strong><code>*</code></li>
-                            <li><strong>capacity</strong><code>*</code></li>
-                            <li><strong>characteristics</strong></li>
-
-                        </ul>
-
                         <form action="{{ route('evacuation-center.file.store') }}" method="post"
                             enctype="multipart/form-data">
                             @csrf
-                            <div class="row  center mt-5">
+                            <div class="row  center mt-3">
                                 <div class="col-md-6 fileUpload">
                                     <input type="file" name="import_file" id="">
 
                                 </div>
                             </div>
-                            <div class="row mt-5 center">
+                            <div class="row mt-3 mb-5  center">
                                 <div class="col-4 ">
                                     <button class="btn btn-primary  " type="submit">{{ __('Import') }}</button>
                                 </div>
                             </div>
                         </form>
+                        <h6><strong>Important note:</strong> The file must contain the following column name.</h6>
+                        <table class="table">
+                            <thead>
+                                <tr class="font-weight-bold">
+                                    <td>Column name</td>
+                                    <td>Required?</td>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="font-weight-bold">evacuation_center_name</td>
+                                    <td>Yes</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">address</td>
+                                    <td>Yes</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">latitude</td>
+                                    <td>Yes</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">longitude</td>
+                                    <td>Yes</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">capacity</td>
+                                    <td>Yes</td>
+
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">characteristics</td>
+                                    <td>No</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+
+                        <h6><strong>Sample Import:</strong></h6>
+                        <img class="" src="/assets/import-sample/evacuation center.png" style="width: 100%; ">
+
+
 
                     </div>
                 </div>
--- resources/views/admin/field_officers_resource/import.blade.php
+++ resources/views/admin/field_officers_resource/import.blade.php
@@ -10,14 +10,13 @@
     <div class="fade-in">
 
         <div class="row center">
-            <div class="col-lg-8 ">
+            <div class="col-lg-10 ">
                 <div class="card">
                     <div class="card-header">
                         <h5>Import Excel to add Field Officers</h5>
 
                     </div>
                     <div class="card-body ">
-                        <h6>To add more than one field officer, you can upload an excel file.</h6>
                         <div class="row">
                             @if(count($errors) > 0)
                             <div class="alert alert-danger col-12 ">
@@ -33,36 +32,79 @@
                             @endif
 
                         </div>
-                        <code>Important note: The file must contain the following column name.</code>
-                        <ul>
-
-                            <li><strong>name</strong><code>*</code></li>
-                            <li><strong>email</strong><code>*</code> - unique</li>
-                            <li><strong>contact_no</strong><code>*</code> - unique</li>
-                            <li><strong>barangay</strong> - required for all barangay captains</li>
-                            <li><strong>designation</strong> - required for all field officers except barangay captains
-                            </li>
-
-                        </ul>
-
-                        <form action="{{ url('/import/field_officers/store') }}" method="post"
-                            enctype="multipart/form-data">
-                            @csrf
-                            <div class="row  center mt-5">
-                                <div class="col-md-6 fileUpload">
-                                    <input type="file" name="import_file" id="">
-
+                        <div>
+                            <form action="{{ url('/import/field_officers/store') }}" method="post"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <div class="row  center mt-3">
+                                    <div class="col-md-6 fileUpload">
+                                        <input type="file" name="import_file" id="">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row mt-5 center">
-                                <div class="col-4 ">
-                                    <button class="btn btn-primary  " type="submit">{{ __('Import') }}</button>
+                                <div class="row mt-3 mb-5 center">
+                                    <div class="col-4 ">
+                                        <button class="btn btn-primary  " type="submit">{{ __('Import') }}</button>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
+
+                        </div>
 
 
+                        <h6><strong>Important note:</strong> The file must contain the following column name.</h6>
+                        <table class="table">
+                            <thead>
+                                <tr class="font-weight-bold">
+                                    <td>Column name</td>
+                                    <td>Required?</td>
+                                    <td>Unique?</td>
+                                    <td>Format</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="font-weight-bold">name</td>
+                                    <td>Yes</td>
+                                    <td>No</td>
+                                    <td>First name MI. Last name</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">email</td>
+                                    <td>Yes</td>
+                                    <td>Yes</td>
+                                    <td>yourname@gmail.com</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">contact_no</td>
+                                    <td>Yes</td>
+                                    <td>Yes</td>
+                                    <td>09xxxxxxxxx</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">officer_type</td>
+                                    <td>Yes</td>
+                                    <td>No</td>
+                                    <td>Barangay Captain, Camp Manager, Courier</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">barangay</td>
+                                    <td>For Barangay Captains only</td>
+                                    <td>Yes</td>
+                                    <td>N/A</td>
+                                </tr>
+                                <tr>
+                                    <td class="font-weight-bold">designation</td>
+                                    <td>Yes, except for Barangay Captains</td>
+                                    <td>No</td>
+                                    <td>N/A</td>
+                                </tr>
+                            </tbody>
+                        </table>
 
-                        </form>
+                        <h6><strong>Sample Import:</strong></h6>
+                        <img class="" src="/assets/import-sample/Field officer.png" style="width: 100%; ">
+
+
 
                     </div>
                 </div>
