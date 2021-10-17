67fe0a88fcb6d02a0141f33d1b4a7c863d855962
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-10-17 12:48:15 +0800

    Admin inventory

--- resources/views/admin/inventory-resource/supplyList.blade.php
+++ resources/views/admin/inventory-resource/supplyList.blade.php
@@ -60,6 +60,17 @@
                 @endif
             </div>
         </div>
+        <div class="row">
+            <div class="col-12">
+                @if(Session::has('error'))
+                <div class="alert alert-danger">{{ Session::get('error') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
+            </div>
+        </div>
 
         <!-- /.row-->
         <div class="row">
