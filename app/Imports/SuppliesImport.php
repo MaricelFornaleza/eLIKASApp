18fa0d766f22f67a9e327fd402a4048a066591da
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-10-17 15:44:05 +0800

    Upload validation

--- app/Imports/SuppliesImport.php
+++ app/Imports/SuppliesImport.php
@@ -91,7 +91,7 @@ class SuppliesImport implements ToCollection, WithHeadingRow
             }
         } else {
             Session::flash('error', 'Supply type not valid');
-            return redirect('/supplies');
+            return redirect('/import/supplies');
         }
     }
 }
