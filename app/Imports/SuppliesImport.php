92ed185f1f1f861e2b6637f4640307f58a736119
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-10-17 15:31:37 +0800

    Uploaad validation

--- app/Imports/SuppliesImport.php
+++ app/Imports/SuppliesImport.php
@@ -23,46 +23,70 @@ class SuppliesImport implements ToCollection, WithHeadingRow
             '*.quantity' => ['required', 'numeric', "min:1"],
             '*.source' => ['required', 'string', 'max:255'],
         ])->validate();
+        $supply_type = [
+            'Clothes',
+            'ESA',
+            'Food Packs',
+            'Hygiene Kit',
+            'Medicine',
+            'Water'
+        ];
+
 
         $user = Auth::user();
 
         $user_inventory_id = User::find($user->id)->user_inventory->id;
-
+        // for checking 
+        $flag = 0;
         foreach ($collection as $row) {
             if ($row->filter()->isNotEmpty()) {
-                $user_inventory_prev_stock = $user->user_inventory()->first();
-                if ($row['supply_type'] == 'Food Packs') {
-                    $user->user_inventory()->update([
-                        'total_no_of_food_packs'                    => ($user_inventory_prev_stock->total_no_of_food_packs + $row['quantity']),
-                    ]);
-                } else if ($row['supply_type'] == 'Water') {
-                    $user->user_inventory()->update([
-                        'total_no_of_water'                         => ($user_inventory_prev_stock->total_no_of_water + $row['quantity']),
-                    ]);
-                } else if ($row['supply_type'] == 'Hygiene Kit') {
-                    $user->user_inventory()->update([
-                        'total_no_of_hygiene_kit'                   => ($user_inventory_prev_stock->total_no_of_hygiene_kit + $row['quantity']),
-                    ]);
-                } else if ($row['supply_type'] == 'Medicine') {
-                    $user->user_inventory()->update([
-                        'total_no_of_medicine'                      => ($user_inventory_prev_stock->total_no_of_medicine + $row['quantity']),
-                    ]);
-                } else if ($row['supply_type'] == 'Clothes') {
-                    $user->user_inventory()->update([
-                        'total_no_of_clothes'                       => ($user_inventory_prev_stock->total_no_of_clothes + $row['quantity']),
-                    ]);
-                } else if ($row['supply_type'] == 'ESA') {
-                    $user->user_inventory()->update([
-                        'total_no_of_emergency_shelter_assistance'  => ($user_inventory_prev_stock->total_no_of_emergency_shelter_assistance + $row['quantity']),
-                    ]);
+                if (in_array($row['supply_type'], $supply_type)) {
+                    $flag = 1;
+                } else {
+                    $flag = 0;
+                    break;
                 }
-                $supply = new Supply();
-                $supply->inventory_id     = $user_inventory_id;
-                $supply->date   = now()->format('F j, Y');
-                $supply->supply_type   = $row['supply_type'];
-                $supply->quantity = $row['quantity'];
-                $supply->source = $row['source'];
-                $supply->save();
+            }
+        }
+
+        if ($flag == 1) {
+            foreach ($collection as $row) {
+                if ($row->filter()->isNotEmpty()) {
+                    $user_inventory_prev_stock = $user->user_inventory()->first();
+
+                    if ($row['supply_type'] == 'Food Packs') {
+                        $user->user_inventory()->update([
+                            'total_no_of_food_packs'                    => ($user_inventory_prev_stock->total_no_of_food_packs + $row['quantity']),
+                        ]);
+                    } else if ($row['supply_type'] == 'Water') {
+                        $user->user_inventory()->update([
+                            'total_no_of_water'                         => ($user_inventory_prev_stock->total_no_of_water + $row['quantity']),
+                        ]);
+                    } else if ($row['supply_type'] == 'Hygiene Kit') {
+                        $user->user_inventory()->update([
+                            'total_no_of_hygiene_kit'                   => ($user_inventory_prev_stock->total_no_of_hygiene_kit + $row['quantity']),
+                        ]);
+                    } else if ($row['supply_type'] == 'Medicine') {
+                        $user->user_inventory()->update([
+                            'total_no_of_medicine'                      => ($user_inventory_prev_stock->total_no_of_medicine + $row['quantity']),
+                        ]);
+                    } else if ($row['supply_type'] == 'Clothes') {
+                        $user->user_inventory()->update([
+                            'total_no_of_clothes'                       => ($user_inventory_prev_stock->total_no_of_clothes + $row['quantity']),
+                        ]);
+                    } else if ($row['supply_type'] == 'ESA') {
+                        $user->user_inventory()->update([
+                            'total_no_of_emergency_shelter_assistance'  => ($user_inventory_prev_stock->total_no_of_emergency_shelter_assistance + $row['quantity']),
+                        ]);
+                    }
+                    $supply = new Supply();
+                    $supply->inventory_id     = $user_inventory_id;
+                    $supply->date   = now()->format('F j, Y');
+                    $supply->supply_type   = $row['supply_type'];
+                    $supply->quantity = $row['quantity'];
+                    $supply->source = $row['source'];
+                    $supply->save();
+                }
             }
         }
     }
