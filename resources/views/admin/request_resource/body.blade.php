7be9e37b21b3b22db2b3fbf99b8047bffd81d24c
Maricel Fornaleza <maformaleza@gbox.adnu.edu.ph>
2021-11-08 13:03:50 +0800

    Fix spaces in request page

--- resources/views/admin/request_resource/body.blade.php
+++ resources/views/admin/request_resource/body.blade.php
@@ -1,5 +1,4 @@
-<table id="requestTable" class="table table-borderless table-hover table-light table-striped"
-    style="width: 100%;">
+<table id="requestTable" class="table table-borderless table-hover table-light table-striped" style="width: 100%;">
     <thead>
         <tr>
             <th>TIME RECEIVED</th>
@@ -23,8 +22,7 @@
                     {{ $delivery_request->evacuation_center_address }}
                 </td>
 
-                <div class="modal fade" id="view" data-backdrop="static"
-                    data-keyboard="false" tabindex="-1"
+                <div class="modal fade" id="view" data-backdrop="static" data-keyboard="false" tabindex="-1"
                     aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
@@ -32,23 +30,20 @@
                                 <h5 class="modal-title" id="staticBackdropLabel">
                                     Details
                                 </h5>
-                                <button type="button" class="close" data-dismiss="modal"
-                                    aria-label="Close"><span
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                                         aria-hidden="true">&times;</span></button>
                             </div>
                             <div class="modal-body">
                                 <div class="row">
                                     <div class="mb-2 col-6">
-                                        <label for="time_received"
-                                            class="col-form-label small ">Time
+                                        <label for="time_received" class="col-form-label small ">Time
                                             received:</label>
                                         <h6 id="time_received" class="font-weight-bold">
                                         </h6>
 
                                     </div>
                                     <div class="mb-2 col-6">
-                                        <label for="id"
-                                            class="col-form-label small ">Request
+                                        <label for="id" class="col-form-label small ">Request
                                             ID:</label>
                                         <span class="badge badge-pill text-white verify">
                                         </span>
@@ -59,16 +54,14 @@
                                 </div>
                                 <div class="row">
                                     <div class="mb-2 col-6">
-                                        <label for="camp_manager"
-                                            class="col-form-label small ">Camp
+                                        <label for="camp_manager" class="col-form-label small ">Camp
                                             Manager:</label>
 
                                         <h6 id="camp_manager" class="font-weight-bold">
                                         </h6>
                                     </div>
                                     <div class="mb-2 col-6">
-                                        <label for="evacuation_center"
-                                            class="col-form-label small ">Evacuation
+                                        <label for="evacuation_center" class="col-form-label small ">Evacuation
                                             Center:</label>
                                         <h6 id="evacuation_center" class="font-weight-bold">
                                         </h6>
@@ -76,8 +69,7 @@
 
                                 </div>
                                 <div class="mb-2 ">
-                                    <label for="note"
-                                        class="col-form-label small ">Note:</label>
+                                    <label for="note" class="col-form-label small ">Note:</label>
                                     <h6 id="note" class="font-weight-bold">
                                     </h6>
                                 </div>
@@ -101,8 +93,7 @@
                                         <small class="label-small">ESA</small>
                                     </div>
                                     <div class="col-4 border p-2">
-                                        <h3 class="pb-0 mb-0"><strong
-                                                id="food_packs"></strong>
+                                        <h3 class="pb-0 mb-0"><strong id="food_packs"></strong>
                                         </h3>
                                         <small class="label-small">FOOD PACKS</small>
                                     </div>
@@ -131,8 +122,7 @@
                                 </div>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary"
-                                    data-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 
                             </div>
                         </div>
@@ -148,30 +138,26 @@
                     </div>
                 </td>
                 <td>
-                    <div class="row">
-                        <div class="mr-4 ">
-                            <button type="button"
-                                class="btn bg-secondary-accent text-white " id="button"
-                                data-toggle="modal" data-target="#view"
-                                data-array="{{json_encode($delivery_request)}}">
+                    <div class="row justify-content-between">
+                        <div class=" ">
+                            <button type="button" class="btn bg-secondary-accent text-white " id="button"
+                                data-toggle="modal" data-target="#view" data-array="{{json_encode($delivery_request)}}">
                                 View
                             </button>
 
                         </div>
-                        <div class="col-4 ">
+                        <div class="">
                             <a href="{{ route('request.approve', ['id' => $delivery_request->id] ) }}"
                                 onclick="return confirm('Are you sure to approve the request?')">
                                 {{-- <img class="c-icon" src="{{ url('icons/sprites/accept-request.svg') }}"
                                 /> --}}
-                                <svg width="25" height="25"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <image
-                                        href="{{ url('icons/sprites/approve-request.svg') }}"
-                                        height="25" width="25" />
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="{{ url('icons/sprites/approve-request.svg') }}" height="25"
+                                        width="25" />
                                 </svg>
                             </a>
                         </div>
-                        <div class="col-4">
+                        <div class="mr-2">
                             {{-- <form action="" method="post">
                                 @csrf
                                 @method("DELETE")
@@ -184,11 +170,9 @@
                             </form> --}}
                             <a href="{{ route('request.admin_decline', ['id' => $delivery_request->id] ) }}"
                                 onclick="return confirm('Are you sure to decline the request?')">
-                                <svg width="25" height="25"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <image
-                                        href="{{ url('icons/sprites/decline-request.svg') }}"
-                                        height="25" width="25" />
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="{{ url('icons/sprites/decline-request.svg') }}" height="25"
+                                        width="25" />
                                 </svg>
                             </a>
                         </div>
@@ -202,12 +186,10 @@
                     </span>
                 </td>
                 <td>
-                    <div class="row">
-                        <div class="mr-4 ">
-                            <button type="button"
-                                class="btn bg-secondary-accent text-white " id="button"
-                                data-toggle="modal" data-target="#view"
-                                data-array="{{json_encode($delivery_request)}}">
+                    <div class="row justify-content-between">
+                        <div class=" ">
+                            <button type="button" class="btn bg-secondary-accent text-white " id="button"
+                                data-toggle="modal" data-target="#view" data-array="{{json_encode($delivery_request)}}">
                                 View
                             </button>
 
@@ -219,11 +201,9 @@
                                 data-evac-id="{{ $delivery_request->evacuation_center_id }}">
                                 {{-- <img class="c-icon" src="{{ url('icons/sprites/accept-request.svg') }}"
                                 /> --}}
-                                <svg width="25" height="25"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <image
-                                        href="{{ url('icons/sprites/assign-courier.svg') }}"
-                                        height="25" width="25" />
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="{{ url('icons/sprites/assign-courier.svg') }}" height="25"
+                                        width="25" />
                                 </svg>
                             </a>
                             <!-- Button trigger modal -->
@@ -232,9 +212,8 @@
                             </button> --}}
 
                             <!-- Modal -->
-                            <div class="modal fade" id="assignModal" tabindex="-1"
-                                role="dialog" aria-labelledby="exampleModalLabel"
-                                aria-hidden="true" data-backdrop="static"
+                            <div class="modal fade" id="assignModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static"
                                 data-keyboard="false">
                                 <div class="modal-dialog modal-lg" role="document">
                                     <div class="modal-content">
@@ -243,12 +222,11 @@
                                             <form method="POST"
                                                 action="{{ route('request.assign_courier', ['id' => $delivery_request->id] ) }}">
                                                 @csrf
-                                                <label for="courier_id"
-                                                    class="mr-3 lead modal-title"
+                                                <label for="courier_id" class="mr-3 lead modal-title"
                                                     id="exampleModalLabel">Assign
                                                     Courier</label>
-                                                <select name="courier_id" id='courier_id'
-                                                    class="w-100 form-control" required>
+                                                <select name="courier_id" id='courier_id' class="w-100 form-control"
+                                                    required>
                                                     {{-- <option value=''>Select User</option> --}}
                                                     {{-- @foreach($couriers as $courier)
                                                 <option value='{{ $courier->id }}'>
@@ -257,12 +235,10 @@
                                                     @endforeach --}}
                                                 </select>
 
-                                                <input type="submit" id="submit-form"
-                                                    class="hidden d-none" />
+                                                <input type="submit" id="submit-form" class="hidden d-none" />
                                             </form>
 
-                                            <button type="button" class="close"
-                                                data-dismiss="modal" aria-label="Close">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
@@ -275,8 +251,7 @@
                                             <button type="button" class="btn btn-secondary"
                                                 data-dismiss="modal">Close</button>
                                             {{-- <button type="button" class="btn btn-warning">Assign</button> --}}
-                                            <label for="submit-form" class="btn btn-warning"
-                                                tabindex="0">Assign</label>
+                                            <label for="submit-form" class="btn btn-warning" tabindex="0">Assign</label>
                                         </div>
                                     </div>
                                 </div>
@@ -285,11 +260,9 @@
                         <div class="col-4">
                             <a href="{{ route('request.cancel', ['id' => $delivery_request->id] ) }}"
                                 onclick="return confirm('Are you sure to cancel the request?')">
-                                <svg width="25" height="25"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <image
-                                        href="{{ url('icons/sprites/decline-request.svg') }}"
-                                        height="25" width="25" />
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="{{ url('icons/sprites/decline-request.svg') }}" height="25"
+                                        width="25" />
                                 </svg>
                             </a>
                         </div>
@@ -304,12 +277,10 @@
                     </span>
                 </td>
                 <td>
-                    <div class="row">
-                        <div class="mr-4 ">
-                            <button type="button"
-                                class="btn bg-secondary-accent text-white " id="button"
-                                data-toggle="modal" data-target="#view"
-                                data-array="{{json_encode($delivery_request)}}">
+                    <div class="row justify-content-between">
+                        <div class="">
+                            <button type="button" class="btn bg-secondary-accent text-white " id="button"
+                                data-toggle="modal" data-target="#view" data-array="{{json_encode($delivery_request)}}">
                                 View
                             </button>
 
@@ -318,11 +289,9 @@
                         <div class="col-4">
                             <a href="{{ route('request.cancel', ['id' => $delivery_request->id] ) }}"
                                 onclick="return confirm('Are you sure to cancel the request?')">
-                                <svg width="25" height="25"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <image
-                                        href="{{ url('icons/sprites/decline-request.svg') }}"
-                                        height="25" width="25" />
+                                <svg width="25" height="25" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="{{ url('icons/sprites/decline-request.svg') }}" height="25"
+                                        width="25" />
                                 </svg>
                             </a>
                         </div>
